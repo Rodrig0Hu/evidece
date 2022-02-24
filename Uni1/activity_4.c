65c09b12f9d9c118d6b59a0bb5be8216e1da55e7
Rodrig0Hu <98335095+Rodrig0Hu@users.noreply.github.com>
2022-02-24 12:33:17 -0600

    Add files via upload

---
+++ Uni1/activity_4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+char firstname[255];
+char lastname[255];
+float age = 0;
+char state[255];
+
+int main(){
+
+printf("welcome to the program\n");
+printf("enter your first name\n");
+scanf("%s", firstname);
+printf("eneter your last name\n");
+scanf("%s", lastname);
+printf("eneter your age\n");
+scanf("%f", &age);
+printf("eneter your mother state\n");
+scanf("%s", state);
+
+
+
+return 0;
+
+
+
+
+
+}

dfc572200e1196069e90f2bf41743608fb66cb1f
Rodrig0Hu <98335095+Rodrig0Hu@users.noreply.github.com>
2022-03-17 20:41:19 -0600

    Add files via upload

---
+++ Unit2/arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (int argc, char** argv){
+int myVar = 25;
+float altura {6} = {1.60, 1.7, 1.8, 1.89, 1.76, 1.75};
+// index: //
+// leer info de arreglo //
+
+printf(" el val0r de arr[2]: %f\n", altura[2]);
+
+return 0;
+
+
+
+
+
+}
